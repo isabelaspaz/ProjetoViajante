62a920676f86daa96e496de14052f6530e432d63
IsabelaPazs2 <isabela.s.paz@gmail.com>
2025-07-05 14:11:43 -0300

    Reiniciando projeto React com create-react-app

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,45 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage/Homepage";
-import Cadastro from "./components/Cadastro/Cadastro";
-import Login from "./components/Login/Login";
-import TelaInicial from "./pages/TelaInicial/TelaInicial";
-import Menu from "./components/Menu/Menu"; // Importando o componente Menu
-import NovaViagem from "./components/Viagem/NovaViagem";
-import Mochilas from "./components/Mochilas/Mochilas"; // Nova Mochila
-import Despesas from "./components/Despesas/Despesas"; // Nova Despesa
-import Configuracoes from "./components/Configuracoes/Configuracoes"; // Configurações
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                {/* Rota pública */}
-                <Route path="/" element={<Homepage />} />
-
-                {/* Rota de Cadastro */}
-                <Route path="/cadastro" element={<Cadastro />} />
-
-                {/* Rota de Login */}
-                <Route path="/login" element={<Login />} />
-
-                {/* Rota para Tela Inicial */}
-                <Route path="/tela-inicial" element={<TelaInicial />} />
-
-                {/* Rota para Nova Viagem */}
-                <Route path="/nova-viagem" element={<NovaViagem />} />
-
-                {/* Rota para Nova Mochila */}
-                <Route path="/nova-mochila" element={<Mochilas />} />
-
-                {/* Rota para Nova Despesa */}
-                <Route path="/nova-despesa" element={<Despesas />} />
-
-                {/* Rota para Configurações */}
-                <Route path="/configuracoes" element={<Configuracoes />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- Frontend/src/components/MensagemFeedback/MensagemFeedback.jsx
+++
@@ -1,5 +0,0 @@
-const MensagemFeedback = ({ mensagem }) => {
-  if (!mensagem) return null;
-  return <p className="mensagem-feedback">{mensagem}</p>;
-};
-export default MensagemFeedback;
